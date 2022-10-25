9d6bc93c85b4e59ee55aecc0ecbfc328298e5047
Tamer <105336574+Tamer173@users.noreply.github.com>
2022-10-25 17:52:14 +0500

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -6,8 +6,8 @@ action = {key = 38, label = 'E', name = 'INPUT_PICKUP'}
 }
 
 Config.Positions = {
-{
-pos = {x = -337.51, y = -137.44, z = 38.38},
-whitelistJobName = 'mechanic'
-}
+    {
+        pos = {x = -337.51, y = -137.44, z = 38.38},
+        whitelistJobName = 'mechanic'
+    },
 }
